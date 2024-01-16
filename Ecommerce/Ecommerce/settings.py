d56bb91d64a81bb51594a33e6e88db502fbc0b2e
hothead <abhishekmr280@gmail.com>
2024-01-16 21:25:31 +0530

    added allowed hosts

--- Ecommerce/Ecommerce/settings.py
+++ Ecommerce/Ecommerce/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!c_5k8z5&h#81@*bjf4vmu^ppit9291-sa4$4ju**p1s7pwa#r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['51.20.4.67']
 
 
 # Application definition
